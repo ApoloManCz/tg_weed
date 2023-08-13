66e050fadde583a5f6176b61233a1e425a4c9f3a
Apolo <75501135+ApoloManCz@users.noreply.github.com>
2023-08-14 01:59:32 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,15 @@
+Config = {}
+
+Config.PickupBlip = true
+Config.DealBlip = true
+Config.SellBlip = true
+
+Config.PickupPos1 = vector3(1943.2850, 5024.2866, 42.4331) -- Coords for pickup canabis
+Config.PickupPos2 = vector3(1946.2668, 5021.1304, 42.1622) -- Coords for pickup canabis
+Config.PickupPos3 = vector3(1945.3445, 5014.6270, 42.0697) -- Coords for pickup canabis
+
+Config.DealNPC = vector3(-466.5268, 6288.1704, 13.6128) -- Coords for deal canabis to weed
+Config.DealNPCHeading = 93.6068 -- Heading of NPC
+
+Config.SellNPC = vector3(-1062.1088, -503.8439, 36.6098) -- Coords for sell weed
+Config.SellNPCHeading = 119.6630 -- Heading of NPC
---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+-----------------------------------
+---https://discord.gg/S5QtKMBZRB---
+-----------------------------------
+
+fx_version 'cerulean'
+game 'gta5'
+
+author 'TG_DEVELOPMENT'
+description 'Drug system by TG_DEV'
+version '1.0.0'
+
+client_scripts {
+    'client/main.lua',
+    'config.lua'
+    
+}
+
+server_scripts {
+    'server/main.lua',
+    'config.lua'
+}
+
+shared_script '@es_extended/imports.lua'
